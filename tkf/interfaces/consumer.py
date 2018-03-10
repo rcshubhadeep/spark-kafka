3ca47b85f20ef36270db554183432f88c219c83d
shubhadeep <shubhadeeproychowdhury@gmail.com>
2018-03-10 19:33:02 +0100

    feat: Adding sliding window

--- tkf/interfaces/consumer.py
+++ tkf/interfaces/consumer.py
@@ -1,10 +1,10 @@
 from tkf.adapters.consumers.consumer import get_consumer
 
 
-def run():
-    ac = get_consumer('simple_spark', 'localhost:9092', ['test2'], app_name='SimpleSpark')
+def consume_data():
+    ac = get_consumer('spark_avro', 'localhost:9092', ['test'], app_name='SimpleSpark')
     ac.start_consuming()
 
 
 if __name__ == "__main__":
-    run()
+    consume_data()
--- tkf/interfaces/producer.py
+++ tkf/interfaces/producer.py
@@ -1,6 +1,11 @@
+import time
+
 from tkf.adapters.producers.producer import get_producer
 from tkf.interfaces.helpers import choose_randomly_from
-from tkf.shared.utils import current_milli_time
+from tkf.shared.utils import current_time_in_millis
+
+BASE_PATH = "/home/shubadeep/kpler/pyproj/kafka-ex/exproj/avro_schemas/{}"
+values_schema_file = "values.avsc"
 
 USERS = {'john': 'we0328nhe9t83', 'sid': '8905fjgf568', 'vasu': '342362hfg764r'}
 TARGETS = ['http://example.com/home',
@@ -20,7 +25,9 @@ REFERRER = ['http://google.com',
 
 
 def produce_data():
-    prdcr = get_producer('test2', producer_type='simple')
+    avro_file = BASE_PATH.format(values_schema_file)
+    prdcr = get_producer('test', producer_type='avro', value_schmea_loc=avro_file)
+    # while True:
     for i in range(10):
         name, cid = choose_randomly_from(USERS)
         visited, _ = choose_randomly_from(TARGETS)
@@ -29,8 +36,9 @@ def produce_data():
                       "visted": visited,
                       "cookieid": cid,
                       "refered_from": refr,
-                      "event_time": current_milli_time()}
+                      "event_time": current_time_in_millis()}
         prdcr.produce_message(event_data)
+    # time.sleep(5)
 
 
 if __name__ == "__main__":
